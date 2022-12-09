b8ba349ef0bbfbd4689ffe46c75e40700ddb237c
Reeth Dasgupta <115037176+reethd@users.noreply.github.com>
2022-12-08 21:18:07 -0500

    added favorites functionality

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ var mealNameContainer = document.getElementById("meal-name");
 var thumbnailContainer = document.getElementById("thumbnail");
 var youtubeLinkContainer = document.getElementById("youtube-link");
 var favoriteMealContainer = document.getElementById("favorite-meal");
-var scrollBoxContainer = document.getElementById("scroll-box");
 var ingredientsListContainer = document.getElementById("ingredients-list");
 var rerollButtonContainer = document.getElementById("reroll");
 var categories = document.getElementById("categories");
 var regions = document.getElementById("regions");
+var loadingIconContainer = document.getElementById("loading-icon");
+var favoritesBtn = document.getElementById("favorites-button");
+var favoritesDropdown = document.getElementById("favorites-dropdown");
 var mealId;
 
 var chosenCategory = "";
@@ -23,7 +25,7 @@ span{
 }
 h1{
   color:blue; 
-  font-size: 72px;
+  font-size: 45px;
 }
 `;
 
@@ -55,6 +57,8 @@ function getRandomMeal() {
       $("#youtubeBtn").remove();
       $("#favoriteMealBtn").remove();
       $("#rerollBtn").remove();
+      $("#meal-header").remove();
+      $("#thumb").remove();
 
       generateRandomMeal(data);
     });
@@ -70,47 +74,59 @@ function generateRandomMeal(data) {
     (chosenCategory == mealCategory && chosenRegion == "") ||
     (mealCategory == chosenCategory && mealRegion == chosenRegion)
   ) {
+    $("#icon-GIF").remove();
+    $("#loading-text").remove();
+
     var mealName = data.meals[0].strMeal;
     var thumbnailLink = data.meals[0].strMealThumb;
     var youtubeLink = data.meals[0].strYoutube;
-    var ingredient;
 
     let youtubeBtnEl = document.createElement("button");
     let favoriteMealBtnEl = document.createElement("button");
     let rerollBtnEl = document.createElement("button");
 
+    let mealNameEl = document.createElement("h2");
+    let thumbnailEl = document.createElement("img");
+
+    $("#meal-name").append(mealNameEl);
+    mealNameEl.textContent = mealName;
+    mealNameEl.setAttribute("id", "meal-header");
+    $("#thumbnail").append(thumbnailEl);
+    thumbnailEl.setAttribute("id", "thumb");
+    thumbnailEl.setAttribute("src", thumbnailLink);
     $("#youtube-link").append(youtubeBtnEl);
     youtubeBtnEl.setAttribute("id", "youtubeBtn");
+    youtubeBtnEl.setAttribute("class", "btn");
     $("#favorite-meal").append(favoriteMealBtnEl);
     favoriteMealBtnEl.setAttribute("id", "favoriteMealBtn");
     $("#reroll").append(rerollBtnEl);
     rerollBtnEl.setAttribute("id", "rerollBtn");
 
-    mealNameContainer.textContent = mealName;
-    thumbnailContainer.setAttribute("src", thumbnailLink);
     youtubeBtnEl.innerHTML =
       " <a href= " +
       youtubeLink +
       '><img src="images/youtubelogo.png" height="50"/></a>';
     favoriteMealBtn.textContent = "Favorite This Meal";
-    scrollBoxContainer.setAttribute(
-      "style",
-      `width: 450px;
-    height: 150px;
-    line-height: 3em;
-    overflow: scroll;
-    padding: 5px;
-    background-color: #fcfadd;
-    color: #714d03;
-    border: 4px double #debb07;`
-    );
-    scrollBoxContainer.innerHTML = `<h4>Ingredients</h4>`;
 
-    favoriteMealBtnEl.setAttribute("class", "exists");
-    rerollBtnEl.setAttribute("class", "exists");
+    favoriteMealBtnEl.setAttribute("class", "exists btn btn-danger");
+    rerollBtnEl.setAttribute("class", "exists btn btn-danger");
     rerollBtnEl.textContent = "Try Something Different";
     mealId = data.meals[0].idMeal;
   } else {
+    var iconEl = document.createElement("img");
+    iconEl.setAttribute("id", "icon-GIF");
+    iconEl.setAttribute(
+      "src",
+      "https://data.whicdn.com/images/124012442/original.gif"
+    );
+    var loadingTextEl = document.createElement("h1");
+    loadingTextEl.setAttribute("id", "loading-text");
+    loadingTextEl.textContent = "Loading...";
+
+    if (loadingIconContainer.childNodes.length === 0) {
+      loadingIconContainer.appendChild(loadingTextEl);
+      loadingIconContainer.appendChild(iconEl);
+    }
     getRandomMeal();
   }
 }
@@ -125,9 +141,49 @@ function saveToFavorites() {
     })
     .then((data) => {
       var savedMeal = data.meals[0].strMeal;
-      localStorage.setItem(savedMeal, savedMeal);
+      if (!favoritesList.includes(savedMeal)) {
+        favoritesList.push(savedMeal);
+        localStorage.setItem("favoritesList", JSON.stringify(favoritesList));
+      }
     });
 }
+
+var favoritesContainer = document.getElementById("favorites-dropdown");
+var favoritesList = [];
+
+function displayFavorites() {
+  favoritesContainer.innerHTML = "";
+
+  for (var i = 0; i < favoritesList.length; i++) {
+    var favoriteItem = favoritesList[i];
+    var li = document.createElement("li");
+    li.textContent = favoriteItem;
+    li.setAttribute("data-index", i);
+
+    var deleteButton = document.createElement("button");
+    deleteButton.textContent = "Remove";
+    deleteButton.setAttribute("class", "btn btn-outline-danger");
+
+    li.appendChild(deleteButton);
+    favoritesContainer.appendChild(li);
+  }
+}
+
+function init() {
+  var storedFavorites = JSON.parse(localStorage.getItem("favoritesList"));
+  if (storedFavorites !== null) {
+    favoritesList = storedFavorites;
+  }
+}
+
+favoritesDropdown.addEventListener("click", function (e) {
+  var element = e.target;
+  if (element.classList.contains("btn") === true) {
+    var index = element.parentElement.getAttribute("data-index");
+    favoritesList.splice(index, 1);
+    localStorage.setItem("favoritesList", JSON.stringify(favoritesList));
+  }
+});
 
 favoriteMealContainer.addEventListener("click", function (e) {
   if (e.target.classList.contains("exists")) {
@@ -142,4 +198,7 @@ rerollButtonContainer.addEventListener("click", function (e) {
   }
 });
 
+init();
+
+favoritesBtn.addEventListener("click", displayFavorites);
 generateBtn.addEventListener("click", getRandomMeal);
